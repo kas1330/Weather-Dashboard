d8002d418fc337cdda2af69c03f7f253f3bf28e6
Kerri Sexton <k.sexton804@gmail.com>
2020-10-10 00:05:19 -0400

    wrote out some directions for what to do

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//Form inputs, prevent default event 
+//Input for city name
+//Get current and future weather for that city via the API- city, date, icon to represent weather, temp, humidity, wind speed, UV index
+//Add city to local storage (array of cities? think of the high scores for the JS quiz)
+//UV index should have a color indicating it's severity
+//Future weather should show 5 days worth- date, icon, temp and humidity
+//When you click on a city in search history show all the info again
+//When you open the app, show the last city you searched for
