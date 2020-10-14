40667861b24083285b74a030711a9bec66d61482
Kerri Sexton <k.sexton804@gmail.com>
2020-10-14 14:32:53 -0400

    now displays city and date at the top of the page

--- script.js
+++ script.js
@@ -8,6 +8,8 @@
 //When you click on a city in search history show all the info again
 //When you open the app, show the last city you searched for
 //1. Take input from box, plug it into the api query
+//2. Put city name and date at top  of weather box. 
+//Get the image depicting the weather, put that below city name
 
 $(document).ready(function(){
 
@@ -15,9 +17,28 @@ $(document).ready(function(){
     $("#search-btn").on("click", function (event) {
         event.preventDefault();
         console.log('Button clicked');
+        var apiKey = 'faa01ea92e85d4bc6a69d8f941aba85c';
 
         var userInput = $('#city-search').val();
         console.log('City is: ', userInput);
+
+        queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            cityName = response.name;
+            console.log('City name is: ', cityName);
+            var today = new Date();
+            // var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            var date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
+            console.log('Date is: ', date);
+            $('#city-name').html(cityName+ ' ' + date);
+
+
+
+        })
     })
 
 
