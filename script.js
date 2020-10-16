1253b21c7ba9b0a7bca85bad69fea8b5a64fa5cd
Kerri Sexton <k.sexton804@gmail.com>
2020-10-15 22:05:29 -0400

    cleaned up code a little

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 
-//Form inputs, prevent default event 
-//Input for city name
-//Get current and future weather for that city via the API- city, date, icon to represent weather, temp, humidity, wind speed, UV index
 //Add city to local storage (array of cities? think of the high scores for the JS quiz)
-//UV index should have a color indicating it's severity
-//Future weather should show 5 days worth- date, icon, temp and humidity
 //When you click on a city in search history show all the info again
 //When you open the app, show the last city you searched for
-//1. Take input from box, plug it into the api query
-//2. Put city name and date at top  of weather box. 
-//Get the image depicting the weather, put that below city name
+
+
 
 $(document).ready(function(){
 
@@ -74,6 +68,8 @@ $(document).ready(function(){
                 // $(currentUvindex).html(response.value);
                 if(uvIndex >= 0 && uvIndex < 3){
                     $('#uv-ind').css("background-color", "green");
+                    $('#uv-ind').css('color', 'white');
+
                 }
                 if(uvIndex >= 3 && uvIndex <= 6){
                     $('#uv-ind').css("background-color", "yellow");
@@ -84,6 +80,7 @@ $(document).ready(function(){
                 }
                 if(uvIndex >= 8 && uvIndex <= 11){
                     $('#uv-ind').css("background-color", "red");
+                    $('#uv-ind').css('color', 'white');
                 }
                 if(uvIndex > 11){
                     $('#uv-ind').css("background-color", "violet");
